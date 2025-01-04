548bad59316e86c3ee3cc7b90d54aced8eef00c6
sabzero <lwazisabelo7@gmail.com>
2025-01-04 14:46:41 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int firstNumber = Convert.ToInt32(Console.ReadLine());
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int AddTwoNumbers(int firstNumber, int secondNumber){
+
+    int total = firstNumber + secondNumber;
+
+    return total;
+}
+
+Console.WriteLine(AddTwoNumbers(firstNumber, secondNumber));
