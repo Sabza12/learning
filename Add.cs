a6b972fab15a14c3442e6e5dfe8438b474d36df8
sabzero <lwazisabelo7@gmail.com>
2025-01-16 18:00:49 +0200

    Add two number problem

---
+++ Add.cs
@@ -0,0 +1,10 @@
+class Add
+{
+    public int AddTwoNumbers(int firstnumber, int secondnumber){
+
+        int total = firstnumber + secondnumber;
+
+        return total;
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿//Empty
+﻿Console.WriteLine("Enter first number");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Add add = new Add();
+Console.WriteLine("Total: " + add.AddTwoNumbers(firstNumber, secondNumber));
